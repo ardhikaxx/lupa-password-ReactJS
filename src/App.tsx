4a5ddff397fb876037dc2838ce45a32a72d78d6d
ardhikaxx <ardhikayanuar58@gmail.com>
2023-11-05 23:51:01 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [isEmailSent, setIsEmailSent] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsEmailSent(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lupa Password</h1>
+        {isEmailSent ? (
+          <div className="reset-message">
+            Email pemulihan kata sandi telah dikirim. Silakan periksa kotak masuk Anda.
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Masukkan alamat email Anda"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type="submit">Kirim Email Pemulihan</button>
+          </form>
+        )}
       </header>
     </div>
   );
